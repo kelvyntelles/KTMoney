36a698039e4e377f1f9c283e8e82b2f3a46f40be
kelvyntelles <kelvyn.telles@gmail.com>
2021-10-25 20:55:07 -0300

    criando componente TransactionsTable

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,7 +10,7 @@ export const GlobalStyle = createGlobalStyle`
         --blue-light: #6933ff;
 
         --text-title: #363f5f;
-        --text-body: ##969cb3;
+        --text-body: #969cb3;
 
         --shape: #ffffff;
     }
